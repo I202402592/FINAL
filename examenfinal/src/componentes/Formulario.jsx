d91c00357254fff986456e56a6e959cadc5f12ba
I202402592 <I202402592@cibertec.edu.pe>
2024-08-27 22:26:22 +0000

    Sólo falta Arreglar el Pie de la página

--- examenfinal/src/componentes/Formulario.jsx
+++ examenfinal/src/componentes/Formulario.jsx
@@ -5,8 +5,8 @@ import { Button, Form } from 'react-bootstrap';
 const Formulario = () => {
     const [nombre, setNombre] = useState('');
     const [apellidos, setApellidos] = useState('');
-    const [celular, setCelular] = useState('');
-    const [lugar, setLugar] = useState('');
+    const [email, setEmail] = useState('');
+    const [contraseña, setContraseña] = useState('');
     const [errors, setErrors] = useState({});
 
     const regexTexto = /^[a-zA-ZÁÉÍÓÚñ\s]+$/;
@@ -17,28 +17,28 @@ const Formulario = () => {
         const newErrors = {};
         if(!nombre) newErrors.nombre = 'El nombre es obligatorio';
         if(!apellidos) newErrors.apellidos = 'Los apellidos son obligatorio';
-        if(!celular) newErrors.celular = 'El celular es obligatorio';
-        if(!lugar) newErrors.lugar = 'El Lugar es obligatorio';
+        if(!email) newErrors.email = 'El Email es obligatorio';
+        if(!contraseña) newErrors.contraseña = 'El Contraseña es obligatorio';
         if(!regexTexto.test(nombre)) {alert('El nombre no es válido')};
         if(!regexTexto.test(apellidos)) {alert('Los apellidos no son válidos')};
-        if(!regexNumero.test(celular)) {alert('Celular no válido')};
-        if(!regexTexto.test(lugar)) {alert('El lugar ingresado no es válido')};
+        if(!regexNumero.test(email)) {alert('El email ingresado no es válido')};
+        if(!regexTexto.test(contraseña)) {alert('La contraseña ingresada no es válida')};
         setErrors(newErrors);
-        if (nombre==='' || apellidos==='' || celular==='' || lugar==='') {
+        if (nombre==='' || apellidos==='' || email==='' || contraseña==='') {
             alert('Los cajas de texto están vacias');
         }
-        if ((regexTexto.test(nombre)===false && nombre!='') || (regexTexto.test(apellidos)===false && apellidos!='') || (regexNumero.test(celular)===false && celular!='') || (regexTexto.test(lugar)===false && lugar!='')) {
+        if ((regexTexto.test(nombre)===false && nombre!='') || (regexTexto.test(apellidos)===false && apellidos!='') || (regexNumero.test(email)===false && email!='') || (regexTexto.test(contraseña)===false && contraseña!='')) {
             alert('No se ha ingresado datos válidos');
             console.log(nombre);
-            alert(`Datos ingresados: { ${nombre}, ${apellidos}, ${celular}, ${lugar} }`);
+            alert(`Datos ingresados: { ${nombre}, ${apellidos}, ${email}, ${contraseña} }`);
         }
 
     }
 
     const cambioNombre = (e) => setNombre(e.target.value);
     const cambioApellido = (e) => setApellidos(e.target.value);
-    const cambioCelular = (e) => setCelular(e.target.value);
-    const cambioLugar = (e) => setLugar(e.target.value);
+    const cambioEmail = (e) => setEmail(e.target.value);
+    const cambioContraseña = (e) => setContraseña(e.target.value);
 
     return (
         <>
@@ -46,8 +46,8 @@ const Formulario = () => {
                 <Form.Group>
                     <Form.Control type="text" value={nombre} onChange={cambioNombre} placeholder="First Name"></Form.Control>
                     <Form.Control type="text" value={apellidos} onChange={cambioApellido} placeholder="Last Name"></Form.Control>
-                    <Form.Control type="text" value={celular} onChange={cambioCelular} placeholder="Email"></Form.Control>
-                    <Form.Control type="text" value={lugar} onChange={cambioLugar} placeholder="Password"></Form.Control>
+                    <Form.Control type="text" value={email} onChange={cambioEmail} placeholder="Email"></Form.Control>
+                    <Form.Control type="text" value={contraseña} onChange={cambioContraseña} placeholder="Password"></Form.Control>
                 </Form.Group>
                 <Button variant="primary" type="submit" >Submit</Button>
             </Form>
